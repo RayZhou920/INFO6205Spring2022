ff49809fab32966a1f5939a9d40f914779f79809
Rui Zhou <zhou_ray@outlook.com>
2022-01-30 17:34:22 -0800

    commit first assignment for check

--- Assignment1/src/com/company/Main.java
+++ Assignment1/src/com/company/Main.java
@@ -24,6 +24,21 @@ public class Main {
 
         String s = "aab";
         System.out.println(reorganizeString(s));
+
+        String order = "cba";
+        String str = "abcd";
+        System.out.println(customSortString(order, str));
+
+        System.out.println(pancakeSort(citations));
+
+        int[] nums = {1,1,2,2,2,3};
+        System.out.println(frequencySort(nums));
+
+        String[] words = new String[]{"i","love","leetcode","i","love","coding"};
+        int k2 = 2;
+
+        System.out.println(topKFrequent(words, 2));
+
     }
 
     // Leetcode 75. Sort Colors
@@ -243,4 +258,152 @@ public class Main {
 
     }
 
+    // Leetcode: 791. Custom Sort String
+    // Time Complexity: ON
+    // Space Complexity: ON（ for StringBuilder )
+    public static String customSortString(String order, String s) {
+        Map<Character, Integer> hashmap = new HashMap<>();
+
+        for (int i = 0; i < s.length(); i++) {
+            hashmap.put(s.charAt(i), hashmap.getOrDefault(s.charAt(i), 0) + 1);
+        }
+
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < order.length(); i++) {
+            if (hashmap.containsKey(order.charAt(i))) {
+                while (hashmap.get(order.charAt(i)) != 0) {
+                    sb.append(order.charAt(i));
+                    hashmap.put(order.charAt(i), hashmap.get(order.charAt(i)) - 1);
+                }
+
+            }
+        }
+
+        for (char c : hashmap.keySet()) {
+            while (hashmap.get(c) != 0) {
+                sb.append(c);
+                hashmap.put(c, hashmap.get(c) - 1);
+            }
+        }
+
+        return sb.toString();
+    }
+
+    // Leetcode: 969. Pancake Sorting
+    // Time Complexity: ON^2
+    // Space Complexity: O1
+    public static List<Integer> pancakeSort(int[] arr) {
+        List<Integer> result = new ArrayList<>();
+
+        if (arr == null || arr.length == 0) return result;
+
+        int end = arr.length;
+        helper(arr, result, end);
+        return result;
+    }
+
+    private static void helper(int[] arr, List<Integer> result, int end) {
+        if (end == 0) {
+            return;
+        }
+
+        // 每轮翻转都要找最大数以及对应的下标：
+        // [3, 2, 4, 1] maxValue=4  maxIndex=2
+        // 两轮翻转：[4, 2, 3, 1]   --->   [1, 3, 2, 4](将4翻转到0的位置，再翻转到末尾)
+        // [1, 3, 2, 4]
+        // 两轮翻转：[3, 1, 2, 4]   --->   [2, 1, 3, 4](将3翻转到0的位置，再翻转到数组第3位)
+        // [2, 1, 3, 4]
+        // [1, 2, 3, 4]
+        int maxValue = 0;
+        int maxIndex = 0;
+        for (int i = 0; i < end; i++) {
+            if (arr[i] > maxValue) {
+                maxValue = arr[i];
+                maxIndex = i;
+            }
+        }
+
+        result.add(maxIndex + 1);
+        reverse(0, maxIndex, arr);
+        result.add(end);
+        reverse(0, end - 1, arr);
+        helper(arr, result, end - 1);
+    }
+
+    private static void reverse(int start, int end, int[] arr) {
+        int left = start;
+        int right = end;
+        while (left < right) {
+            int temp = arr[right];
+            arr[right] = arr[left];
+            arr[left] = temp;
+            left++;
+            right--;
+        }
+    }
+
+    // Leetcode: 1636. Sort Array by Increasing Frequency
+    // Time Complexity: ON
+    // Space Complexity: OlogN (for the heap)
+    public static int[] frequencySort(int[] nums) {
+        if (nums == null || nums.length == 0) return new int[0];
+
+        Map<Integer, Integer> hashmap = new HashMap<>();
+        for (int i : nums) {
+            hashmap.put(i, hashmap.getOrDefault(i, 0) + 1);
+        }
+        // [1,1,2,2,2,3]
+        // [1 - 2; 2 - 3; 3 - 1]
+
+        PriorityQueue<Integer> queue = new PriorityQueue<>((a, b) -> hashmap.get(a) == hashmap.get(b) ? b - a : hashmap.get(a) - hashmap.get(b));
+        queue.addAll(hashmap.keySet());
+
+        int[] result = new int[nums.length];
+        int index = 0;
+        while (!queue.isEmpty()) {
+            int cur = queue.poll();
+            while (hashmap.get(cur) != 0) {
+                result[index] = cur;
+                index++;
+                hashmap.put(cur, hashmap.get(cur) - 1);
+            }
+        }
+
+        return result;
+    }
+
+    // Leetcode: 692. Top K Frequent Words
+    // Time Complexity: ONlogK
+    // Space Complexity: OlogK for the heap
+    public static List<String> topKFrequent(String[] words, int k) {
+        List<String> result = new ArrayList<>();
+        Map<String, Integer> hashmap = new HashMap<>();
+        // 最小堆堆内排序：
+        // 1. 如果词频不同，按照从小到大生序排序 -> 最小堆
+        // 2. 如果词频相同，按照字母表顺序进行降序排序（倒序）（即字母表在前的在堆内，字母表在后的在堆上）
+        PriorityQueue<String> queue = new PriorityQueue<>((a, b) -> hashmap.get(a) == hashmap.get(b) ? b.compareTo(a) : hashmap.get(a) - hashmap.get(b));
+
+        for (String s : words) {
+            hashmap.put(s, hashmap.getOrDefault(s, 0) + 1);
+        }
+
+        for (String s : hashmap.keySet()) {
+            // 词频
+            queue.add(s);
+            if (queue.size() > k) {
+                queue.poll();
+            }
+
+        }
+
+        while (!queue.isEmpty()) {
+            result.add(queue.poll());
+        }
+
+        // 最后需要反转list，因为queue 首先poll出来的是堆顶元素，即刚好第k个 / 字母表在后的
+        // 题目要求是从高 -> 低，字母表在前的在列表前面，所以需要反转列表
+        Collections.reverse(result);
+
+        return result;
+    }
 }
